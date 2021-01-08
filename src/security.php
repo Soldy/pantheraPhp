58e5ef8eb573abefaf34e3e08107a601be5510f3
Soldy <4786022+Soldy@users.noreply.github.com>
2021-01-08 21:33:20 +0000

    security logic
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ src/security.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Soldy\PanteraPhp;
+// because every body need a triats.
+
+trait SqlSecurity {
+   /*  Always double check every thing.
+   * @param {string} input text 
+   * @rotected
+   * @return {string} safe string 
+   */
+    protected function security(string $string) : string
+    {
+        return htmlspecialchars($string, ENT_QUOTES); // not a real full safe
+    }
+   /* the functions may have multiple inputs
+   * @param {array} input 
+   * @rotected
+   * @return {string} safe string  // the output is a ready to push string.
+   */
+    protected function securitys(array $var_array) : string // should have to replace this function
+    {
+        $out = "";
+        for($i=0;count($var_array)>$i;$i++){
+            if($i>0) $out.=", ";
+            $out.="'".$this->security($var_array[$i])."'";
+        }
+        return $out;
+    }
+}
+
