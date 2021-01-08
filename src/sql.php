7873da24f83e57acac70ce3398d0c8cc21956281
Soldy <4786022+Soldy@users.noreply.github.com>
2021-01-08 21:38:42 +0000

    first implementation
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ src/sql.php
@@ -0,0 +1,71 @@
+<?php
+
+
+namespace Soldy\PanteraPhp;
+
+use Soldy\PantheraPhp\SqlAbstract;
+use Soldy\PantheraPhp\SqlSecurity;
+
+
+class Sql extends SqlAbstract 
+{
+    use SqlSecurity;
+    /* this funcion call the functions on the sql
+     *  
+     * @param {string}
+     * @param {array} variables
+     * @public
+     * @return {mixed} always come back with the first line. 
+     * 
+     *
+     */
+    public function queryProcedure(string $procedure, array $var_array ) 
+    {
+        // I use . because it's faster
+        $sql = '`'. $procedure. '`('; 
+        $sql.= $this->securitys($var_array); 
+        $sql.=')';
+        // And its not a prepare syntax jet... because that not a live code
+        // That should brake the solid o.
+        return $this->query(
+           'CALL '. $sql 
+        );
+    }
+    /* this funcion call the functions on the sql
+     *  
+     * @param {string}
+     * @param {array} variables
+     * @public
+     * @return {mixed} always come back with the first line. 
+     * 
+     *
+     */
+    public function queryFunction(string $func, array $var_array ) 
+    {
+        // I use . because it's faster
+        $sql = '`'. $func. '`('; 
+        $sql.= $this->securitys($var_array); 
+        $sql.=')';
+        // And its not a prepare syntax jet... because that not a live code
+        // That should brake the solid o.
+        return $this->query(
+           'SELECT '. $sql 
+        )[0][$sql]; 
+    }
+    function __construct(){
+        $this->connect();
+    }
+    function __construct(string $user, string $password, string $host, string $db)
+    {
+         $this->config(
+             $user,
+             $password, 
+             $host, 
+             $db
+         );
+
+    }
+
+}
+
+
